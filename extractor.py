409d8a75a97db4cc6434813d15f10d623142b8fd
Harisgitama <raihanardana567@gmail.com>
2018-08-24 08:06:19 +0700

    first commit

---
+++ extractor.py
@@ -0,0 +1,13 @@
+print "KuBiK Code'-'"
+tanya = raw_input('Nama File :')
+file = open(tanya+'','r').read()
+jawab = file.split('&')
+hijau = '\033[32;1m'
+putih = '\033[37;1m'
+for x in jawab:
+	print hijau +'Hasil >>\n' + putih + x
+psn = """
+GINI AJA GKBISA BUAT DASAR AJG!1!1
+GW HEKER.NIH ASU!1!1
+HEKER KUBIK!1!1 DSR LOKNOT
+-HG"""
